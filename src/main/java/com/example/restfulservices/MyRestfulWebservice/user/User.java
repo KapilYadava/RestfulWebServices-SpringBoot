ce7b1976280d114e65c3f432d2cdf437476bd738
kapilyadav <kapilyadav@kapils-macbook-pro.local>
2019-11-22 16:34:43 +0530

    Adding gcloud plugin

--- src/main/java/com/example/restfulservices/MyRestfulWebservice/user/User.java
+++ src/main/java/com/example/restfulservices/MyRestfulWebservice/user/User.java
@@ -1,26 +1,39 @@
 package com.example.restfulservices.MyRestfulWebservice.user;
 
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
 @ApiModel(description = "This is about user description")
+@Entity
 public class User {
 
+	@Id
+	@GeneratedValue
 	private Integer id;
 	@Size(min = 2, message = "Name should have atleast 2 or more characters")
 	@ApiModelProperty(notes = "Name should have atleast 2 or more characters")
 	private String name;
-	@JsonIgnore
+	
 	@Past(message = "dob must be in past")
 	@ApiModelProperty(notes = "dob should be in the past")
 	private Date dob;
+	
+	@OneToMany(mappedBy="user")
+	private List<Post> post;
+	
+	public User() {
+		
+	}
 	
 	public User(Integer id, String name, Date dob) {
 		super();
@@ -47,7 +60,12 @@ public class User {
 	public void setDob(Date dob) {
 		this.dob = dob;
 	}
-	
-	
-	
+
+	public List<Post> getPost() {
+		return post;
+	}
+
+	public void setPost(List<Post> post) {
+		this.post = post;
+	}
 }
--- src/main/java/com/example/restfulservices/MyRestfulWebservice/user/UserResources.java
+++ src/main/java/com/example/restfulservices/MyRestfulWebservice/user/UserResources.java
@@ -2,6 +2,7 @@ package com.example.restfulservices.MyRestfulWebservice.user;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -25,11 +26,23 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 public class UserResources {
 	
 	@Autowired
-	UserDaoService service;
+	private UserDaoService service;
+	
+	@Autowired
+	private UserRepository repository;
+	
+	@Autowired
+	private PostRepository postRepository;
+	
 	
 	@GetMapping(path = "/users")
 	public List<User> findAll() {
 		return service.findAll();
+	}
+	
+	@GetMapping(path = "jpa/users")
+	public List<User> findAllJpa() {
+		return repository.findAll();
 	}
 	
 	@GetMapping(path = "/users/{id}")
@@ -56,11 +69,42 @@ public class UserResources {
 		return resource;
 	}
 
+	@GetMapping(path = "jpa/users/{id}")
+	public Resource<User> findUserJpa(@PathVariable int id) {
+		Optional<User> user = repository.findById(id);
+		if(!user.isPresent())
+			throw new UserNotFoundException("id =" + id);
+		
+		//"all-users", SERVER_PATH + "/users"
+				//retrieveAllUsers
+				Resource<User> resource = new Resource<User>(user.get());
+				
+				ControllerLinkBuilder linkTo = 
+						linkTo(methodOn(this.getClass()).findAllJpa());
+				
+				resource.add(linkTo.withRel("all-users"));
+				
+				linkTo = linkTo(methodOn(this.getClass()).deleteUserJpa(id));
+				
+				resource.add(linkTo.withRel("delete-user"));
+				//HATEOAS
+				
+		
+		return resource;
+	}
+
+	
 	@DeleteMapping(path = "/users/{id}")
 	public ResponseEntity<Object> deleteUser(@PathVariable int id) {
 		User deletedUser = service.deleteUser(id);
 		if (deletedUser == null)
 			throw new UserNotFoundException("id = " +id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@DeleteMapping(path = "jpa/users/{id}")
+	public ResponseEntity<Object> deleteUserJpa(@PathVariable int id) {
+		repository.deleteById(id);
 		return ResponseEntity.noContent().build();
 	}
 	
@@ -71,6 +115,41 @@ public class UserResources {
 		URI location = ServletUriComponentsBuilder.
 			fromCurrentRequest().path("/{id}")
 			.buildAndExpand(savedUser.getId())
+			.toUri();
+		return ResponseEntity.created(location).build();
+	}
+	
+	@PostMapping(path = "jpa/users")
+	public ResponseEntity<Object> createUserUsingJpa(@Valid @RequestBody User user) {
+		User savedUser =repository.save(user);
+		
+		URI location = ServletUriComponentsBuilder.
+			fromCurrentRequest().path("/{id}")
+			.buildAndExpand(savedUser.getId())
+			.toUri();
+		return ResponseEntity.created(location).build();
+	}
+
+	@GetMapping(path = "jpa/users/{id}/post")
+	public List<Post> findAllUserPost(@PathVariable int id) {
+		Optional<User> user = repository.findById(id);
+		if(!user.isPresent())
+			throw new UserNotFoundException("id =" + id);
+		
+		
+		return user.get().getPost();
+	}
+	
+	@PostMapping(path = "jpa/users/{id}/post")
+	public ResponseEntity<Object> createUser(@PathVariable int id, @RequestBody Post post) {
+		Optional<User> user = repository.findById(id);
+		if(!user.isPresent())
+			throw new UserNotFoundException("id =" + id);
+		post.setUser(user.get());
+		Post savePost =postRepository.save(post);
+		URI location = ServletUriComponentsBuilder.
+			fromCurrentRequest().path("/{id}")
+			.buildAndExpand(savePost.getId())
 			.toUri();
 		return ResponseEntity.created(location).build();
 	}
